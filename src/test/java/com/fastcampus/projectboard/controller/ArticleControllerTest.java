77e2f93fe192a9a3b57bb06c1ea22d2d987981e9
dohyoup <dohyoup@naver.com>
2024-11-09 05:52:35 +0900

    로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.fastcampus.projectboard.controller;
 
+import com.fastcampus.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -45,8 +48,8 @@ class ArticleControllerTest {
 
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Disabled("구현 중")
     @Test
     public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
         mvc.perform(get("/articles/search"))
@@ -56,8 +59,8 @@ class ArticleControllerTest {
 
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 전용 페이지 - 정상 호출")
+    @Disabled("구현 중")
     @Test
     public void givenNothing_whenRequestingArticleHashTagSearchView_thenReturnArticleHashTagSearchView() throws Exception {
         mvc.perform(get("/articles/search-hashtag"))
